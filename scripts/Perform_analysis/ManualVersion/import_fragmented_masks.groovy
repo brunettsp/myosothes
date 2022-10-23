5746ca9faab5025aa4f240af17536acc0ac7f652
Marie Reinbigler <marie.reinbigler@telecom-sudparis.eu>
2022-10-23 07:15:42 +0200

    Add nested masks removal in import_fragmented_masks scripts

--- scripts/Perform_analysis/ManualVersion/import_fragmented_masks.groovy
+++ scripts/Perform_analysis/ManualVersion/import_fragmented_masks.groovy
@@ -61,6 +61,26 @@ for (File elt: currentImport) {
 }
 
 clearAllObjects()
+
+x_int = []
+y_int = []
+
+
+for (int i=0; i< xpositions.size;i++) { 
+    x_int.add( xpositions[i].toInteger());
+}
+
+for (int i=0; i< ypositions.size;i++) { 
+    y_int.add( ypositions[i].toInteger());
+}
+
+
+min_x = x_int.min() 
+min_y = y_int.min()
+
+max_x = x_int.max() 
+max_y = y_int.max()
+
 double downsample = 1 // TO CHANGE (if needed)
 ImagePlane plane = ImagePlane.getDefaultPlane()
 def imageData = getCurrentImageData()
@@ -70,14 +90,9 @@ def server = imageData.getServer()
 height = server.getHeight()
 width = server.getWidth()
 
-
-def tile_size = 2000
-
-lines = []
-
-def i_x = 0
-def i_y = 0
-
+def overlap = 200
+def threshold = 0.85
+def tile_size = 512
 
 int index = 0
 
@@ -130,5 +145,109 @@ removeObjects(detections, true)
 addObjects(newAnnotations)
 
 }
+
+
+
+annotations = getAnnotationObjects()
+
+annotations.each {
+    roi = it.getROI()
+    bx = roi.getBoundsX()
+    by = roi.getBoundsY()
+    bh = roi.getBoundsHeight()
+    bw = roi.getBoundsWidth()
+    it.getMeasurementList().putMeasurement("bx", bx)
+    it.getMeasurementList().putMeasurement("by", by)
+    it.getMeasurementList().putMeasurement("bw", bh)
+    it.getMeasurementList().putMeasurement("bh", bw)
+
+}
+
+to_remove = []
+for (int x=min_x; x<=max_x; x+=tile_size){
+    for (int y=min_y; y<=max_y; y+=tile_size){
+        def inOverlap = getAnnotationObjects().findAll {((measurement(it, 'bx') >= x && measurement(it, 'bx') <= x+overlap) 
+        || (measurement(it, 'bx') +   measurement(it, 'bw') >= x && measurement(it, 'bx') +   measurement(it, 'bw') <= x+overlap))
+        && ((measurement(it, 'by') +   measurement(it, 'bh') >= y && measurement(it, 'by') +   measurement(it, 'bh') <= y+overlap)
+        || (measurement(it, 'by') >= y && measurement(it, 'by') <= y+overlap) )
+        }        
+        inOverlap.each{
+            bx = it.getMeasurementList().getMeasurementValue("bx")
+            by = it.getMeasurementList().getMeasurementValue("by")
+            bw = it.getMeasurementList().getMeasurementValue("bw")
+            bh = it.getMeasurementList().getMeasurementValue("bh")
+
+            annots = getAnnotsWithinBB(inOverlap, bx, by, bw, bh)
+            nannots = (annots.size())
+            if (nannots>0){
+                for (i in 0..nannots-1){
+                    if (annots[i] != it){
+                        ann01_roi = it.getROI()
+                        ann02_roi = annots[i].getROI()
+                        ann01_geo = it.getROI().getGeometry()
+                        ann02_geo = annots[i].getROI().getGeometry()
+                        plane = ann01_roi.getImagePlane()
+                    
+                        intersect_geo = ann01_geo.intersection(ann02_geo)
+                        intersect_roi = GeometryTools.geometryToROI(intersect_geo, plane)
+                        intersect_annotation = PathObjects.createAnnotationObject(intersect_roi)
+                        intersect_area = intersect_annotation.getROI().getArea()
+                    
+                       
+                       
+                       
+                        ann01_area = ann01_roi.getArea()
+                        ann02_area = ann02_roi.getArea()
+                        
+             
+                        
+                        if (ann01_area > ann02_area){
+                            if (intersect_area/ann02_area > threshold) {
+                                to_remove.add(annots[i])
+                            }
+                        }
+                        else {
+                           if (intersect_area/ann01_area > threshold) {
+                               to_remove.add(it)
+                               break;
+                           }
+                        }
+                        
+                   }  
+                }
+            
+            }
+
+        
+          } 
+        
+        
+        
+        
+    }
+}
+
+removeObjects(to_remove, true)
+
+
+
+def getAnnotsWithinBB(annotations, bx1, by1, bw1, bh1){
+    x1 = bx1
+    y1 = by1
+    x2 = bx1+bw1
+    y2 = by1+bh1
+    annots = []
+    annotations.each{
+        x3 = it.getMeasurementList().getMeasurementValue("bx")
+        y3 = it.getMeasurementList().getMeasurementValue("by")
+        x4 = x3 + it.getMeasurementList().getMeasurementValue("bw")
+        y4 = y3 + it.getMeasurementList().getMeasurementValue("bh")
+        if (!((x1 > x4)|(x2 < x3)|(y1 > y4)|(y2 < y3))) {
+            annots << it          
+        }
+    }
+    return(annots)
+}
+
 
 
